cf21fe600fc5ca03939948e65f842de5b692ec97
evan2002 <evanwaang2020@gmail.com>
2024-09-20 21:33:32 -0700

    blackjack component

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import twaLogo from './assets/tapps.png'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-import WebApp from '@twa-dev/sdk'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Blackjack from './features/blackjack/blackjack';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App: React.FC = () => {
   return (
-    <>
-      <div>
-        <a href="https://ton.org/dev" target="_blank">
-          <img src={twaLogo} className="logo" alt="TWA logo" />
-        </a>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>TWA + Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      {/*  */}
-      <div className="card">
-        <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
-            Show Alert
-        </button>
-      </div>
-    </>
+    <Router>
+      <Routes>
+
+        {/* CHANGE LATER, redirecting all routes to blackjack app for now*/}
+        <Route path="/" element={<Navigate to="/blackjack" replace />} />
+        
+        {/* Blackjack app route */}
+        <Route path="/blackjack/*" element={<Blackjack />} />
+
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/features/blackjack/blackjack.tsx
@@ -0,0 +1,6 @@
+const Blackjack: React.FC = () => {
+    return <h1>Welcome to blackjack nigga</h1>;
+  }
+
+
+export default Blackjack;
